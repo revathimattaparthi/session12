fc06669e49ceaa1143c3dac6549791bcc2578d2b
Revathii <srevathi036@gmail.com>
2016-09-16 18:25:29 +0530

    add /products

--- server1.js
+++ server1.js
@@ -1,24 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 8080;
-var todos =[{
-	id:1,
-	description:'Hiii Revathi',
-	completed:false
-},{
-	id:2,
-	description:'How are you?',
-	completed:false
-}, {
-	id:3,
-	description:'What are u doing?',
-	completed:true
-}];
+var todoNextId = 1;
+todos =[];
 
 app.get('/', function(req, res){
 	res.send('Todo API Root');
 });
-
+ app.use(bodyParser.json());
 // get/todos
 app.get('/todos', function(req, res){
 	res.json(todos);
@@ -41,6 +31,14 @@ app.get('/todos/:id', function(req, res){
 	 } else {
 	 	res.status(404).send();
 	 }
+});
+
+app.post('/todos', function(req, res){
+	var body = req.body;
+	 body.id = todoNextId++;
+	todos.push(body);
+	res.json(body);
+
 });
 
 app.listen(PORT, function(){
